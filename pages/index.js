b6d39bf2d9d40beda14b9901ffbe9d41beec1097
nathansearles <nsearles@gmail.com>
2020-09-07 11:41:12 -0700

    roughed in footer

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "../components/Head";
 import Section from "../components/Section";
 import { Container, Row, Column } from "../components/Grid";
 import Topbar from "../components/Topbar";
+import Footer from "../components/Footer";
 
 import styles from "../styles/Home.module.scss";
 
@@ -10,27 +11,23 @@ export default function Home() {
   return (
     <>
       <Head title="Welcome" />
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Topbar />
-          <Section align="center">
-            <Container>
-              <Row align="center" textAlign={{ xs: "center", sm: "left" }}>
-                <Column
-                  columns={{ xs: 14, sm: 11, md: 10 }}
-                  offsets={{ sm: 1, md: 1 }}
-                >
-                  <h1>There’s more to health than steps & weight</h1>
-                </Column>
-              </Row>
-            </Container>
-          </Section>
-        </main>
 
-        <footer className={styles.footer}>
-          <a href="#"></a>
-        </footer>
-      </div>
+      <main className={styles.main}>
+        <Topbar />
+        <Section align="center">
+          <Container>
+            <Row align="center" textAlign={{ xs: "center", sm: "left" }}>
+              <Column
+                columns={{ xs: 14, sm: 11, md: 10 }}
+                offsets={{ sm: 1, md: 1 }}
+              >
+                <h1>There’s more to health than steps & weight</h1>
+              </Column>
+            </Row>
+          </Container>
+        </Section>
+      </main>
+      <Footer />
     </>
   );
 }
