618aa533ea6f34157d2a114e648c14504db38637
nathansearles <nsearles@gmail.com>
2020-09-07 12:12:39 -0700

    roughed in side menu

--- components/Topbar/index.js
+++ components/Topbar/index.js
@@ -1,16 +1,27 @@
 import { Container, Row, Column } from "../Grid";
+import { useState } from "react";
 import Logo from "../Logo";
+import Menu from "../Menu";
 
 import styles from "./topbar.module.scss";
 
 const Topbar = () => {
+  const [isOpen, toggleOpen] = useState(false);
+
+  const toggleMenu = () => {
+    toggleOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.topbar}>
-      <button>Menu</button>
-      <Logo />
-      <button>Account</button>
-      <button>Cart</button>
-    </div>
+    <>
+      <div className={styles.topbar}>
+        <button onClick={() => toggleMenu()}>Menu</button>
+        <Logo />
+        <button>Account</button>
+        <button>Cart</button>
+      </div>
+      <Menu active={isOpen} toggle={() => toggleMenu()} />
+    </>
   );
 };
 
