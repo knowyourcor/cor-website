d1b26269ab5f4c61c3e6dacefa5a06c495fcc8be
nathansearles <nsearles@gmail.com>
2020-09-08 15:26:45 -0700

    resolved build issues

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "../components/Head";
 import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
 import Modules from "../components/Modules";
-import { homepage } from "../data/temp";
 import Alert from "../components/alert";
 
 import { getHomepageData } from "../lib/api";
