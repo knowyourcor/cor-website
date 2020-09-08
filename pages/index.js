92ac9d2829cf001dc826082bd9b926e793197abd
nathansearles <nsearles@gmail.com>
2020-09-08 14:03:35 -0700

    Roughed in connection to Prismic API with preview

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,17 @@ import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
 import Modules from "../components/Modules";
 import { homepage } from "../data/temp";
+import Alert from "../components/alert";
+
+import { getHomepageData } from "../lib/api";
 
 import styles from "../styles/Home.module.scss";
 
-export default function Home() {
+export default function Index({ preview, data }) {
   return (
     <>
-      <Head title="Welcome" />
+      <Head title={data.meta_title} />
+      <Alert preview={preview} />
       <main>
         <Topbar />
         <Modules data={homepage} />
@@ -17,4 +21,12 @@ export default function Home() {
       <Footer />
     </>
   );
+}
+
+export async function getStaticProps({ preview = false, previewData }) {
+  const data = await getHomepageData(previewData);
+  return {
+    props: { preview, data },
+    revalidate: 1, // In seconds
+  };
 }
