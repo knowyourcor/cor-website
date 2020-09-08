595ae77593142bf2db5994789924621ec7b95c5b
nathansearles <nsearles@gmail.com>
2020-09-08 15:26:45 -0700

    resolved build issues

--- pages/shop.js
+++ pages/shop.js
@@ -5,7 +5,6 @@ import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
 
 import Modules from "../components/Modules";
-import { shop } from "../data/temp";
 
 // import styles from "../styles/Shop.module.scss";
 
@@ -15,7 +14,16 @@ export default function Shop() {
       <Head title="Shop" />
       <main>
         <Topbar />
-        <Modules data={shop} />
+        <Section>
+          <Container>
+            <Row align="center" textAlign={{ xs: "center", sm: "left" }}>
+              <Column columns={{ xs: 14, sm: 12 }} offsets={{ sm: 1 }}>
+                <h1>Shop</h1>
+                <p>Placeholder for Shop.</p>
+              </Column>
+            </Row>
+          </Container>
+        </Section>
       </main>
       <Footer />
     </>
