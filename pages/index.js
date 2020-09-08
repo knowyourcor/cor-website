03a42ab561c186feb236e3dc91357be3fe5de5dd
nathansearles <nsearles@gmail.com>
2020-09-08 15:30:06 -0700

    resolved issue with component name

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "../components/Head";
 import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
 import Modules from "../components/Modules";
-import Alert from "../components/alert";
+import Alert from "../components/Alert";
 
 import { getHomepageData } from "../lib/api";
 
