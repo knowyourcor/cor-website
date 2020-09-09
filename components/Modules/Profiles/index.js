9a859b4df4974f6f5f006c15cd7fb2149a57a917
nathansearles <nsearles@gmail.com>
2020-09-09 12:04:55 -0700

    Roughed in profiles module with cms data

--- components/Modules/Profiles/index.js
+++ components/Modules/Profiles/index.js
@@ -1,16 +1,49 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./profiles.module.scss";
 
-const Profiles = ({ content, background }) => {
+const Tab = (tabData) => {
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <div className={styles.tab}>
+      <Row align="center" textAlign={{ xs: "center" }}>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          <RichText render={tabData.tab_name} />
+          <RichText render={tabData.profile_name} />
+          <RichText render={tabData.profile_description} />
+          <RichText render={tabData.profile_text} />
+          <RichText render={tabData.profile_type} />
+          <p>{tabData.profile_score}</p>
+        </Column>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          <img
+            src={tabData.image.xxl.url}
+            alt={tabData.image.alt}
+            className={styles.image}
+          />
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+const Profiles = ({ primary, fields }) => {
+  return (
+    <Section
+      fullScreen
+      backgroundColor={primary.background_color}
+      align="center"
+    >
       <Container>
         <Row align="center" textAlign={{ xs: "center" }}>
-          <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <div dangerouslySetInnerHTML={{ __html: content }} />
+          <Column columns={{ xs: 14 }}>
+            {primary.headline && <RichText render={primary.headline} />}
           </Column>
         </Row>
+
+        {fields.map((field, index) => {
+          return <Tab {...field} key={`tab_${index}`} />;
+        })}
       </Container>
     </Section>
   );
