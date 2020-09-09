da83d1ca53443cfee801fb95dbd6e00ae63efa01
nathansearles <nsearles@gmail.com>
2020-09-09 11:57:31 -0700

    Roughed in tabs module with CMS data

--- components/Modules/Tabs/index.js
+++ components/Modules/Tabs/index.js
@@ -1,14 +1,43 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./tabs.module.scss";
 
-const Tabs = ({ primary, background }) => {
+const Tab = ({ tab_name, text, image }) => {
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <div className={styles.tab}>
+      <Row align="center" textAlign={{ xs: "center" }}>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          <RichText render={tab_name} />
+          <RichText render={text} />
+        </Column>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          <img src={image.xxl.url} alt={image.alt} className={styles.image} />
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+const Tabs = ({ primary, fields }) => {
+  return (
+    <Section
+      fullScreen
+      backgroundColor={primary.background_color}
+      align="center"
+    >
       <Container>
         <Row align="center" textAlign={{ xs: "center" }}>
-          <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.headline[0].text}</h3>
+          <Column columns={{ xs: 14 }}>
+            {primary.headline && <RichText render={primary.headline} />}
+          </Column>
+        </Row>
+
+        <Row align="center" textAlign={{ xs: "center" }}>
+          <Column columns={{ xs: 14 }}>
+            {fields.map((field, index) => {
+              return <Tab {...field} key={`tab_${index}`} />;
+            })}
           </Column>
         </Row>
       </Container>
