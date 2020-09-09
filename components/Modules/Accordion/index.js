2f8b2de3d5b46257edae92a1308fe8af7e111c1c
nathansearles <nsearles@gmail.com>
2020-09-09 12:16:05 -0700

    Rouged in accordion module with CMS data

--- components/Modules/Accordion/index.js
+++ components/Modules/Accordion/index.js
@@ -1,16 +1,38 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./accordion.module.scss";
 
-const Accordion = ({ primary, background }) => {
+const Item = ({ title, text }) => {
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <div className={styles.item}>
+      <Row align="center" textAlign={{ xs: "center" }}>
+        <Column columns={{ xs: 14 }}>
+          <RichText render={title} />
+          <RichText render={text} />
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+const Accordion = ({ primary, fields }) => {
+  return (
+    <Section
+      fullScreen
+      backgroundColor={primary.background_color}
+      align="center"
+    >
       <Container>
         <Row align="center" textAlign={{ xs: "center" }}>
           <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.headline[0].text}</h3>
+            <RichText render={primary.headline} />
           </Column>
         </Row>
+
+        {fields.map((field, index) => {
+          return <Item {...field} key={`item_${index}`} />;
+        })}
       </Container>
     </Section>
   );
