d2bc634ca0936ac0039c215f89ce70fde90d0675
nathansearles <nsearles@gmail.com>
2020-09-09 11:30:16 -0700

    Roughed in hero carousel with CMS data

--- components/Modules/CarouselHero/index.js
+++ components/Modules/CarouselHero/index.js
@@ -1,16 +1,48 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./carouselHero.module.scss";
 
-const CarouselHero = ({ primary, background }) => {
+// primary {
+//   headline
+//   text
+// }
+// fields {
+//   image
+//   headline
+//   text
+// }
+
+// TO DO: add link
+
+const Slide = ({ image, headline, number, video_source }) => {
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <div className={styles.slide}>
+      <Row align="center" textAlign={{ xs: "center" }}>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          {image && <img src={image.xxl.url} alt={image.alt} />}
+        </Column>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          {headline && <RichText render={headline} />}
+          {number && number}
+          {video_source && video_source}
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+const CarouselHero = ({ primary, fields }) => {
+  return (
+    <Section
+      fullScreen
+      backgroundColor={primary.background_color}
+      align="center"
+    >
       <Container>
-        <Row align="center" textAlign={{ xs: "center" }}>
-          <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.headline[0].text}</h3>
-          </Column>
-        </Row>
+        {fields.map((field, index) => {
+          return <Slide {...field} key={`slide_${index}`} />;
+        })}
       </Container>
     </Section>
   );
