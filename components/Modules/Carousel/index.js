e747a8e05e26ea817c0f8a6e7fb4429dcca62c94
nathansearles <nsearles@gmail.com>
2020-09-09 12:19:49 -0700

    Roughed in carousel module with CMS data

--- components/Modules/Carousel/index.js
+++ components/Modules/Carousel/index.js
@@ -1,16 +1,42 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./carousel.module.scss";
 
-const Carousel = ({ primary, background }) => {
+const Slide = ({ headline, text, image }) => {
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <div className={styles.tab}>
+      <Row align="center" textAlign={{ xs: "center" }}>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          <RichText render={headline} />
+          <RichText render={text} />
+        </Column>
+        <Column columns={{ xs: 14, sm: 7 }}>
+          <img src={image.xxl.url} alt={image.alt} className={styles.image} />
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+const Carousel = ({ primary, fields }) => {
+  return (
+    <Section
+      fullScreen
+      backgroundColor={primary.background_color}
+      align="center"
+    >
       <Container>
         <Row align="center" textAlign={{ xs: "center" }}>
-          <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.headline[0].text}</h3>
+          <Column columns={{ xs: 14 }}>
+            {primary.headline && <RichText render={primary.headline} />}
+            {primary.text && <RichText render={primary.text} />}
           </Column>
         </Row>
+
+        {fields.map((field, index) => {
+          return <Slide {...field} key={`slide_${index}`} />;
+        })}
       </Container>
     </Section>
   );
