3301e1995012e18b9402895da191471d36d914b7
nathansearles <nsearles@gmail.com>
2020-09-09 12:11:52 -0700

    Roughed in quote module with CMS data

--- components/Modules/Quote/index.js
+++ components/Modules/Quote/index.js
@@ -1,14 +1,27 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./quote.module.scss";
 
-const Quote = ({ primary, background }) => {
+const Quote = ({ primary }) => {
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <Section
+      fullScreen
+      backgroundColor={primary.background_color}
+      align="center"
+    >
       <Container>
         <Row align="center" textAlign={{ xs: "center" }}>
           <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.quote[0].text}</h3>
+            <blockquote>
+              {primary.quote[0].text}
+              <footer>—{primary.author_name[0].text}</footer>
+            </blockquote>
+            <img
+              src={primary.author_portrait.xxl.url}
+              alt={primary.author_portrait.alt}
+              className={styles.authorPortrait}
+            />
           </Column>
         </Row>
       </Container>
