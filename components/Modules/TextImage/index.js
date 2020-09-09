a1a598797588647fea45f4f4259edc13b1f82414
nathansearles <nsearles@gmail.com>
2020-09-09 11:35:56 -0700

    Roughed in text/image module with CMS data

--- components/Modules/TextImage/index.js
+++ components/Modules/TextImage/index.js
@@ -1,14 +1,38 @@
+import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import styles from "./textImage.module.scss";
 
-const TextImage = ({ primary, background }) => {
+// primary {
+//   headline
+//   text
+//   image
+//   background_color
+//   alignment
+//   overlap_text_and_image
+// }
+
+const TextImage = ({ primary }) => {
+  // primary.overlap_text_and_image
+  // primary.alignment
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <Section fullScreen backgroundColor={primary.background} align="center">
       <Container>
         <Row align="center" textAlign={{ xs: "center" }}>
-          <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.headline[0].text}</h3>
+          <Column columns={{ xs: 14 }}>
+            <RichText render={primary.headline} />
+          </Column>
+        </Row>
+        <Row align="center" textAlign={{ xs: "center" }}>
+          <Column columns={{ xs: 14, sm: 7 }}>
+            <RichText render={primary.text} />
+          </Column>
+          <Column columns={{ xs: 14, sm: 7 }}>
+            <img
+              src={primary.image.xxl.url}
+              alt={primary.image.alt}
+              className={styles.image}
+            />
           </Column>
         </Row>
       </Container>
