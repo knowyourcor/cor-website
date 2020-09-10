354977ec233c7acc763bfab6495437687d5de28b
nathansearles <nsearles@gmail.com>
2020-09-09 21:44:06 -0700

    Setup dynamic routes and data queries using the page slug

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,27 +1,30 @@
-import { useRouter } from "next/router";
+import { RichText } from "prismic-reactjs";
 import Head from "../components/Head";
-import Section from "../components/Section";
-import { Container, Row, Column } from "../components/Grid";
 import Topbar from "../components/Topbar";
 import Footer from "../components/Footer";
+import Section from "../components/Section";
+import { Container, Row, Column } from "../components/Grid";
+import Modules from "../components/Modules";
+import Alert from "../components/Alert";
+
+import { getPageData, getAllPagesWithSlug } from "../lib/api";
 
 // import styles from "../styles/Page.module.scss";
 
-export default function Page() {
-  const router = useRouter();
-  const { slug } = router.query;
-
+export default function Page({ preview, pageData }) {
   return (
     <>
-      <Head title="Page Title" />
+      <Head title={pageData.meta_title} />
+      <Alert preview={preview} />
       <main>
         <Topbar />
         <Section>
           <Container>
             <Row align="center" textAlign={{ xs: "center", sm: "left" }}>
               <Column columns={{ xs: 14, sm: 12 }} offsets={{ sm: 1 }}>
-                <h1>{slug}</h1>
-                <p>Placeholder for catch all pages.</p>
+                <RichText render={pageData.page_title} />
+                {/* TODO hook this up to Module */}
+                <RichText render={pageData.body[0].primary.text} />
               </Column>
             </Row>
           </Container>
@@ -30,4 +33,23 @@ export default function Page() {
       <Footer />
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const allPages = await getAllPagesWithSlug();
+  return {
+    paths:
+      allPages?.map(({ node }) => {
+        return { params: { slug: `${node._meta.uid}` } };
+      }) || [],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ preview = false, previewData, params }) {
+  const pageData = await getPageData(params.slug, previewData);
+  return {
+    props: { preview, pageData },
+    revalidate: 1,
+  };
 }
