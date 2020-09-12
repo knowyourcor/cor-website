d0ecbed2f8f8052686c7f8519ae7528b9c660400
nathansearles <nsearles@gmail.com>
2020-09-11 18:00:03 -0700

    Added Profiles tabs functionality

--- components/Modules/Profiles/index.js
+++ components/Modules/Profiles/index.js
@@ -1,33 +1,69 @@
+// import { RichText } from "prismic-reactjs";
+// import Section from "../../Section";
+// import { Container, Row, Column } from "../../Grid";
+// import styles from "./profiles.module.scss";
+
+// const Tab = (tabData) => {
+//   return (
+//     <div className={styles.tab}>
+//       <Row align="center" textAlign={{ xs: "center" }}>
+//         <Column columns={{ xs: 14, sm: 7 }}>
+//           <RichText render={tabData.tab_name} />
+//           <RichText render={tabData.profile_name} />
+//           <RichText render={tabData.profile_description} />
+//           <RichText render={tabData.profile_text} />
+//           <RichText render={tabData.profile_type} />
+//           <p>{tabData.profile_score}</p>
+//         </Column>
+//         <Column columns={{ xs: 14, sm: 7 }}>
+//           <img
+//             src={tabData.image.xxl.url}
+//             alt={tabData.image.alt}
+//             className={styles.image}
+//           />
+//         </Column>
+//       </Row>
+//     </div>
+//   );
+// };
+
+// const Profiles = ({ primary, fields }) => {
+//   return (
+//     <Section
+//       fullScreen
+//       backgroundColor={primary.background_color}
+//       align="center"
+//     >
+//       <Container>
+//         <Row align="center" textAlign={{ xs: "center" }}>
+//           <Column columns={{ xs: 14 }}>
+//             {primary.headline && <RichText render={primary.headline} />}
+//           </Column>
+//         </Row>
+
+//         {fields.map((field, index) => {
+//           return <Tab {...field} key={`tab_${index}`} />;
+//         })}
+//       </Container>
+//     </Section>
+//   );
+// };
+
+// export default Profiles;
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { RichText } from "prismic-reactjs";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
+import Item from "./Item";
+import Content from "./Content";
+
 import styles from "./profiles.module.scss";
 
-const Tab = (tabData) => {
-  return (
-    <div className={styles.tab}>
-      <Row align="center" textAlign={{ xs: "center" }}>
-        <Column columns={{ xs: 14, sm: 7 }}>
-          <RichText render={tabData.tab_name} />
-          <RichText render={tabData.profile_name} />
-          <RichText render={tabData.profile_description} />
-          <RichText render={tabData.profile_text} />
-          <RichText render={tabData.profile_type} />
-          <p>{tabData.profile_score}</p>
-        </Column>
-        <Column columns={{ xs: 14, sm: 7 }}>
-          <img
-            src={tabData.image.xxl.url}
-            alt={tabData.image.alt}
-            className={styles.image}
-          />
-        </Column>
-      </Row>
-    </div>
-  );
-};
+export default function Profiles({ primary, fields }) {
+  const [isOpen, setIsOpen] = useState("tab-0");
 
-const Profiles = ({ primary, fields }) => {
   return (
     <Section
       fullScreen
@@ -41,12 +77,31 @@ const Profiles = ({ primary, fields }) => {
           </Column>
         </Row>
 
-        {fields.map((field, index) => {
-          return <Tab {...field} key={`tab_${index}`} />;
-        })}
+        <div className={styles.tabs}>
+          <Row align="center">
+            <Column columns={{ xs: 14 }}>
+              <motion.ul layout className={styles.labels}>
+                {fields.map((item, index) => (
+                  <Item
+                    key={`tab-${index}`}
+                    isOpen={`tab-${index}` === isOpen}
+                    label={item.tab_name[0].text}
+                    openTab={() => setIsOpen(`tab-${index}`)}
+                  />
+                ))}
+              </motion.ul>
+            </Column>
+          </Row>
+
+          <div className={styles.tabContent}>
+            {fields.map((item, index) => (
+              <AnimatePresence exitBeforeEnter key={`tab-${index}`}>
+                {`tab-${index}` === isOpen && <Content {...item} />}
+              </AnimatePresence>
+            ))}
+          </div>
+        </div>
       </Container>
     </Section>
   );
-};
-
-export default Profiles;
+}
