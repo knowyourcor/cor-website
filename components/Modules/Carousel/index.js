c9714d09b6b4a3c0eba8dd220b72c3f14aff5da3
nathansearles <nsearles@gmail.com>
2020-09-14 16:04:48 -0700

    Refined carousel

--- components/Modules/Carousel/index.js
+++ components/Modules/Carousel/index.js
@@ -8,9 +8,17 @@ import styles from "./carousel.module.scss";
 const Slide = ({ headline, text, image }) => {
   return (
     <div className={styles.tab}>
-      <img src={image.xxl.url} alt={image.alt} className={styles.image} />
-      <RichText render={headline} />
-      <RichText render={text} />
+      <div className={styles.portrait}>
+        <img src={image.xxl.url} alt={image.alt} className={styles.image} />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 754 754">
+          <path d="M754.001 754h-754V0h754v754zM103.566 465.85a286.381 286.381 0 0040.549 79.672 289.175 289.175 0 0062.294 62.883 286.054 286.054 0 00170.595 56.1v87.5h.029a380.085 380.085 0 0038.313-1.936 374.429 374.429 0 00334.721-334.721 379.967 379.967 0 000-76.684A374.429 374.429 0 00415.346 3.943a380.348 380.348 0 00-76.684 0A374.424 374.424 0 003.941 338.664 380.84 380.84 0 002.001 377a374.716 374.716 0 0018.331 115.891l83.23-27.039z" />
+        </svg>
+      </div>
+
+      <div className={styles.content}>
+        <RichText render={headline} />
+        <RichText render={text} />
+      </div>
     </div>
   );
 };
@@ -32,8 +40,13 @@ const Carousel = ({ primary, fields }) => {
         spacing: 30,
         slidesPerView: 3,
       },
+      "(min-width: 1024px)": {
+        spacing: 30,
+        slidesPerView: 3,
+        centered: true,
+      },
       "(min-width: 1280px)": {
-        spacing: 45,
+        spacing: 160,
         slidesPerView: 4,
         centered: true,
       },
@@ -46,6 +59,7 @@ const Carousel = ({ primary, fields }) => {
         <Row align="center" textAlign={{ xs: "left" }}>
           <Column columns={{ xs: 14, md: 6 }} offsets={{ md: 1 }}>
             {primary.headline[0].text && <RichText render={primary.headline} />}
+
             {primary.text[0].text && <RichText render={primary.text} />}
           </Column>
         </Row>
