4efc97b03b1b7c322d5ec02ebc8bde87d86c067f
nathansearles <nsearles@gmail.com>
2020-09-15 09:44:17 -0700

    clean up

--- components/Modules/Accordion/index.js
+++ components/Modules/Accordion/index.js
@@ -9,19 +9,18 @@ import styles from "./accordion.module.scss";
 
 export default function Accordion({ primary, fields }) {
   const [expanded, setExpanded] = useState("item-0");
-  console.log(primary);
 
-  function fontColorFromBackground(bgColor, lightColor, darkColor) {
+  function themeFromBackground(bgColor, lightTheme, darkTheme) {
     var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
     var r = parseInt(color.substring(0, 2), 16); // hexToR
     var g = parseInt(color.substring(2, 4), 16); // hexToG
     var b = parseInt(color.substring(4, 6), 16); // hexToB
-    return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
+    return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkTheme : lightTheme;
   }
 
   const backgroundColor = primary.background_color;
 
-  const getThemeFromBackground = fontColorFromBackground(
+  const getThemeFromBackground = themeFromBackground(
     backgroundColor,
     "light",
     "dark"
