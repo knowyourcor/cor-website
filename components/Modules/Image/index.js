6e2e80bd9c2d4358d5f13269aa21338bcc0b7ef1
nathansearles <nsearles@gmail.com>
2020-09-16 14:12:32 -0700

    Image component updated with responsive image support

--- components/Modules/Image/index.js
+++ components/Modules/Image/index.js
@@ -1,12 +1,11 @@
+import Picture from "../../Picture";
 import styles from "./image.module.scss";
 
 const Image = ({ primary }) => {
   return (
-    <img
-      className={styles.image}
-      src={primary.image.xxl.url}
-      alt={primary.image.alt}
-    />
+    <div className={styles.image}>
+      <Picture image={primary.image} />
+    </div>
   );
 };
 
