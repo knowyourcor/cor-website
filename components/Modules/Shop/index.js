959de4eaad573cd01e98297910022e5d2c9c7807
nathansearles <nsearles@gmail.com>
2020-09-17 16:46:37 -0700

    Roughed in Shop component

--- components/Modules/Shop/index.js
+++ components/Modules/Shop/index.js
@@ -1,19 +1,37 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
 import Section from "../../Section";
-import { Container, Row, Column } from "../../Grid";
+import { Container } from "../../Grid";
+import Item from "./Item";
+import Panel from "./Panel";
+
 import styles from "./shop.module.scss";
 
-const Shop = ({ primary, background }) => {
+export default function Shop({ fields }) {
+  const [isOpen, setIsOpen] = useState("tab-0");
+
   return (
-    <Section fullScreen backgroundColor={background} align="center">
+    <Section align="center">
       <Container>
-        <Row align="center" textAlign={{ xs: "center" }}>
-          <Column columns={{ xs: 14, sm: 11 }} offsets={{ sm: 1 }}>
-            <h3>{primary.headline[0].text}</h3>
-          </Column>
-        </Row>
+        <div className={styles.shop}>
+          <motion.ul layout className={styles.tabLabels}>
+            {fields.map((item, index) => (
+              <Item
+                key={`tab-${index}`}
+                isOpen={`tab-${index}` === isOpen}
+                label={item.tab_name[0].text}
+                openTab={() => setIsOpen(`tab-${index}`)}
+              />
+            ))}
+          </motion.ul>
+          {fields.map(
+            (item, index) =>
+              `tab-${index}` === isOpen && (
+                <Panel key={`tab-${index}`} {...item} />
+              )
+          )}
+        </div>
       </Container>
     </Section>
   );
-};
-
-export default Shop;
+}
