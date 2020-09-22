ff7f5a8fca39d0d05d00a97a96e943babb2a02b6
nathansearles <nsearles@gmail.com>
2020-09-22 12:11:09 -0700

    added container to text module

--- components/Modules/Text/index.js
+++ components/Modules/Text/index.js
@@ -1,12 +1,22 @@
 import { RichText } from "prismic-reactjs";
+import Section from "../../Section";
+import { Container, Row, Column } from "../../Grid";
 
 import styles from "./text.module.scss";
 
 const Text = ({ primary }) => {
   return (
-    <div className={styles.text}>
-      <RichText render={primary.text} />
-    </div>
+    <Section align="center">
+      <Container>
+        <Row align="center">
+          <Column columns={{ xs: 14, sm: 12 }} offsets={{ sm: 1 }}>
+            <div className={styles.text}>
+              <RichText render={primary.text} />
+            </div>
+          </Column>
+        </Row>
+      </Container>
+    </Section>
   );
 };
 
