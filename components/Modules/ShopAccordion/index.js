01d4f88c4d2b3c85351f8da8777d8398343c8c34
nathansearles <nsearles@gmail.com>
2020-09-22 12:52:17 -0700

    Added image switching to shop accordion

--- components/Modules/ShopAccordion/index.js
+++ components/Modules/ShopAccordion/index.js
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import { RichText } from "prismic-reactjs";
-import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 import Picture from "../../Picture";
@@ -9,85 +8,97 @@ import Item from "./Item";
 import styles from "./shopAccordion.module.scss";
 
 export default function ShopAccordion({ primary, fields }) {
-  const ImageBased = () => {
-    const [expanded, setExpanded] = useState("item-0");
+  const [expanded, setExpanded] = useState("item-0");
 
-    const backgroundColor = primary.background_color
-      ? primary.background_color
-      : "transparent";
+  const [imageData, setImageData] = useState();
 
-    return (
-      <Section
-        style={{
-          backgroundColor: backgroundColor,
-        }}
-      >
-        <Container>
-          <Row justify="center">
-            <Column columns={{ xs: 14, sm: 10, md: 7, lg: 7 }}>
-              {primary.image && (
-                <Picture image={primary.image} className={styles.image} />
-              )}
-            </Column>
-            <Column
-              columns={{ xs: 14, md: 6, lg: 5 }}
-              offsets={{ md: 1, lg: 1 }}
-            >
-              <div className={styles.accordion}>
-                <motion.div className={styles.items}>
-                  {fields.map((data, index) => (
-                    <Item
-                      key={`item-${index}`}
-                      isExpanded={`item-${index}` === expanded}
-                      expandItem={() => setExpanded(`item-${index}`)}
-                      data={data}
-                      index={index}
-                    />
-                  ))}
-                </motion.div>
-              </div>
-            </Column>
-          </Row>
-        </Container>
-      </Section>
-    );
-  };
-  const TextBased = () => {
-    const [expanded, setExpanded] = useState(null);
+  const backgroundColor = primary.background_color
+    ? primary.background_color
+    : "transparent";
 
-    return (
-      <Section
-        style={{
-          backgroundColor: primary.background_color
-            ? primary.background_color
-            : "transparent",
-        }}
-      >
-        <Container>
-          <Row>
-            <Column
-              columns={{ xs: 14, sm: 12, lg: 10 }}
-              offsets={{ sm: 1, lg: 2 }}
-            >
-              <div className={styles.accordion}>
-                <motion.div className={styles.items}>
-                  {fields.map((data, index) => (
-                    <Item
-                      key={`item-${index}`}
-                      isExpanded={`item-${index}` === expanded}
-                      expandItem={() => setExpanded(`item-${index}`)}
-                      data={data}
-                      index={index}
-                    />
-                  ))}
-                </motion.div>
-              </div>
-            </Column>
-          </Row>
-        </Container>
-      </Section>
-    );
+  const handleImageChange = (data) => {
+    setImageData(data);
   };
 
-  return primary.image ? <ImageBased /> : <TextBased />;
+  useEffect(() => {
+    setImageData(fields[0]?.image);
+  }, []);
+
+  const imageTransition = {
+    opacity: {
+      duration: 0.45,
+      ease: [0.465, 0.183, 0.153, 0.946],
+    },
+    y: {
+      duration: 0.75,
+      ease: [0.465, 0.183, 0.153, 0.946],
+    },
+  };
+
+  const imageVariant = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: imageTransition,
+    },
+    hidden: {
+      opacity: 0,
+      y: "-8%",
+      transition: imageTransition,
+    },
+    exit: {
+      opacity: 0,
+      y: "8%",
+      transition: imageTransition,
+    },
+  };
+
+  return (
+    <Section
+      style={{
+        backgroundColor: backgroundColor,
+      }}
+    >
+      <Container>
+        <Row justify="center">
+          <Column columns={{ xs: 14, sm: 10, md: 7, lg: 7 }}>
+            {imageData && (
+              <div className={styles.primaryImage}>
+                <AnimatePresence initial={false}>
+                  <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    variants={imageVariant}
+                    key={imageData.url}
+                  >
+                    <Picture image={imageData} className={styles.image} />
+                  </motion.div>
+                </AnimatePresence>
+              </div>
+            )}
+          </Column>
+          <Column columns={{ xs: 14, md: 6, lg: 5 }} offsets={{ md: 1, lg: 1 }}>
+            <div className={styles.accordion}>
+              <motion.div className={styles.items}>
+                {fields.map((data, index) => (
+                  <Item
+                    key={`item-${index}`}
+                    isExpanded={`item-${index}` === expanded}
+                    expandItem={() => {
+                      setExpanded(`item-${index}`);
+                      handleImageChange(data.image);
+                    }}
+                    data={data}
+                    index={index}
+                    changeImage={handleImageChange}
+                  />
+                ))}
+              </motion.div>
+            </div>
+          </Column>
+        </Row>
+      </Container>
+    </Section>
+  );
 }
