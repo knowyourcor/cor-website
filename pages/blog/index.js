bcb0cb9fca46d3a309c9dfe2390e6aa1e3ad7811
kaye-upliftcx <kaye@upliftcx.com>
2021-04-07 20:13:29 +0800

    Blog: posts category section ( desktop to responsive )

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -19,14 +19,12 @@ export default function Blog({
       title="Blog"
       preview={preview}
     >
-      <div className={styles.postSection}>
+      <div className={styles.sectionPost}>
         <Container>
           <div className={styles.postWrapper}>
             {pageData.map((item, i) => {
               let data = item.node
               let date = moment(data.date).format('DD MMMM, YYYY')
-
-              console.log(data.excerpt[0].text)
 
               return (
                 <div className={styles.postHolder}>
@@ -46,7 +44,7 @@ export default function Blog({
                   <div className={styles.postContent}>
                     <h2 className={styles.title}>{RichText.asText(data.title)}</h2>
                     <div className={styles.wrapper}>
-                      <p className={styles.subtitle}>{RichText.asText(data.subtitle)}</p>
+                      <p className={styles.category}>{RichText.asText(data.subtitle)}</p>
                       <p className={styles.date}>{date}</p>
                     </div>
                     {i === 0 && <p>{data.excerpt[0].text}</p>}
@@ -54,6 +52,50 @@ export default function Blog({
                 </div>
               )
             })}
+          </div>
+        </Container>
+      </div>
+      <div className={styles.sectionPostCategory}>
+        <Container>
+          <div className={styles.contentWrapper}>
+            <div className={styles.categoryWrapper}>
+              <div className={styles.selectedCategory}>Diet</div>
+              <select className={styles.selectCategory} value="Category">
+                <option value="Category">Category</option>
+                <option value="Diet">Diet</option>
+                <option value="Health">Health</option>
+              </select>
+            </div>
+            <div className={styles.postWrapper}>
+              {pageData.map((item, i) => {
+                let data = item.node
+                let date = moment(data.date).format('DD MMMM, YYYY')
+
+                return (
+                  <div className={styles.postHolder}>
+                    <Link href={'/blog/' + data._meta.uid}>
+                      <a>
+                        <div className={styles.imageWrapper}>
+                          <img
+                            src={data.content[0].image.url}
+                            className={styles.image}
+                            alt=""
+                          />
+                        </div>
+                      </a>
+                    </Link>
+                    <div className={styles.wrapper}>
+                      <p className={styles.category}>{RichText.asText(data.subtitle)}</p>
+                      <p className={styles.date}>{date}</p>
+                    </div>
+                    <h2 className={styles.title}>{RichText.asText(data.title)}</h2>
+                  </div>
+                )
+              })}
+            </div>
+            <div className={styles.buttonHolder}>
+              <button className={styles.buttonShow}>Show More</button>
+            </div>
           </div>
         </Container>
       </div>
