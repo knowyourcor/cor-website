a4cce6029a7f5fee72801f3006376c6d9072457d
kaye-upliftcx <kaye@upliftcx.com>
2021-04-07 16:29:03 +0800

    blog page - WIP

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,9 @@
 import { RichText } from "prismic-reactjs";
 import Link from "next/link"
+import moment from "moment"
 
 import Layout from "../../components/Layout"
+import { Container } from "../../components/Grid"
 
 import styles from "../../styles/blog/blog.module.scss"
 
@@ -9,7 +11,6 @@ import { getBlogData } from "../../lib/api"
 
 export default function Blog({
   preview,
-  posts,
   pageData
 }) {
   return (
@@ -18,41 +19,50 @@ export default function Blog({
       title="Blog"
       preview={preview}
     >
-      <ul>
-        {pageData.map((item, i) => {
-          let itemNode = item.node
-          return (
-            <li key={i}>
-              <Link href={'/blog/' + itemNode._meta.uid}>
-                <a><RichText render={itemNode.title} /></a>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-      {/* <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            <Link href={'/blog/' + post.id}>
-              <a><RichText render={pageData.title} /></a>
-            </Link>
-          </li>
-        ))}
-      </ul> */}
+      <Container>
+        <div className={styles.postWrapper}>
+          {pageData.map((item, i) => {
+            let data = item.node
+            let date = moment(data.date).format('DD MMMM, YYYY')
+
+            return (
+              <div className={styles.postHolder}>
+                <div className={styles.postImage}>
+                  <Link href={'/blog/' + data._meta.uid}>
+                    <a>
+                      <div className={styles.imageWrapper}>
+                        <img
+                          src={data.content[0].image.url}
+                          className={styles.image}
+                          alt=""
+                        />
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+                <div className={styles.postContent}>
+                  <h2 className={styles.title}>{RichText.asText(data.title)}</h2>
+                  <div className={styles.wrapper}>
+                    <p className={styles.subtitle}>{RichText.asText(data.subtitle)}</p>
+                    <p className={styles.date}>{date}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </Container>
     </Layout>
   );
 }
 
 export async function getStaticProps({ preview = false, previewData }) {
   const pageData = await getBlogData(previewData);
-  const res = await fetch('http://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
 
   return {
     props: {
       preview,
       pageData,
-      posts: data.slice(0, 4)
     },
     revalidate: 1, // In seconds
   }
