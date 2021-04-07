3159dcef647899ad4a12a805e32f3604a456e225
kaye-upliftcx <kaye@upliftcx.com>
2021-04-07 18:42:50 +0800

    Blog: posts section ( desktop to responsive )

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -19,39 +19,44 @@ export default function Blog({
       title="Blog"
       preview={preview}
     >
-      <Container>
-        <div className={styles.postWrapper}>
-          {pageData.map((item, i) => {
-            let data = item.node
-            let date = moment(data.date).format('DD MMMM, YYYY')
+      <div className={styles.postSection}>
+        <Container>
+          <div className={styles.postWrapper}>
+            {pageData.map((item, i) => {
+              let data = item.node
+              let date = moment(data.date).format('DD MMMM, YYYY')
 
-            return (
-              <div className={styles.postHolder}>
-                <div className={styles.postImage}>
-                  <Link href={'/blog/' + data._meta.uid}>
-                    <a>
-                      <div className={styles.imageWrapper}>
-                        <img
-                          src={data.content[0].image.url}
-                          className={styles.image}
-                          alt=""
-                        />
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-                <div className={styles.postContent}>
-                  <h2 className={styles.title}>{RichText.asText(data.title)}</h2>
-                  <div className={styles.wrapper}>
-                    <p className={styles.subtitle}>{RichText.asText(data.subtitle)}</p>
-                    <p className={styles.date}>{date}</p>
+              console.log(data.excerpt[0].text)
+
+              return (
+                <div className={styles.postHolder}>
+                  <div className={styles.postImage}>
+                    <Link href={'/blog/' + data._meta.uid}>
+                      <a>
+                        <div className={styles.imageWrapper}>
+                          <img
+                            src={data.content[0].image.url}
+                            className={styles.image}
+                            alt=""
+                          />
+                        </div>
+                      </a>
+                    </Link>
+                  </div>
+                  <div className={styles.postContent}>
+                    <h2 className={styles.title}>{RichText.asText(data.title)}</h2>
+                    <div className={styles.wrapper}>
+                      <p className={styles.subtitle}>{RichText.asText(data.subtitle)}</p>
+                      <p className={styles.date}>{date}</p>
+                    </div>
+                    {i === 0 && <p>{data.excerpt[0].text}</p>}
                   </div>
                 </div>
-              </div>
-            )
-          })}
-        </div>
-      </Container>
+              )
+            })}
+          </div>
+        </Container>
+      </div>
     </Layout>
   );
 }
