5a66eb356b19eac2e2fa7db65fea57ed630c1750
maricon-upliftcx <maricon@upliftcx.com>
2021-04-07 15:15:39 +0800

    Blog Post: Dynamic the content and Fixed Responsive View

--- pages/blog/Content/index.js
+++ pages/blog/Content/index.js
@@ -1,3 +1,4 @@
+import { RichText } from "prismic-reactjs";
 import Image from "next/image"
 import { Container } from "../../../components/Grid"
 
@@ -6,35 +7,32 @@ import styles from "./content.module.scss"
 const Index = ({ props }) => {
   return (
     <>
-    {props.map((item, i) => (
-      <div key={i} className={styles.postContent}>
-        {item.image &&
-          <div className={styles.imageWrapper}>
-            <Image
-              src={item.image}
-              className={styles.image}
-              alt=""
-              layout="fill"
-              objectFit="cover"
-              objectPosition= "0 50%"
-            />
-          </div>
-        }
-        <div className={styles.contentText}>
-          <Container>
-            <div className={styles.contentWrapper}>
-              <div className={styles.contentHolder}>
-                <h2 className={styles.heading}>{item.heading}</h2>
-              </div>
-              <div className={styles.contentHolder}>
-                <p>{item.p}</p>
-                {item.quote && <p className={styles.quoteText}>"{item.quote}"</p>}
-              </div>
+      {props.map((item, i) => (
+        <div key={i} className={styles.postContent}>
+          {item.image &&
+            <div className={styles.imageWrapper}>
+              <div className={styles.bgImage} style={{ backgroundImage: `url(${item.image.url})` }}></div>
+              {/* <img className={styles.image} src={item.image.url} /> */}
             </div>
-          </Container>
+          }
+          <div className={styles.contentText}>
+            <Container>
+              <div className={styles.contentWrapper}>
+                <div className={styles.contentHolder}>
+                  {/* <h2 className={styles.heading}>{item.heading}</h2> */}
+                  <RichText render={item.heading} />
+                </div>
+                <div className={styles.contentHolder}>
+                  {/* <p>{item.p}</p> */}
+                  <RichText render={item.paragraph} />
+                  {item.quote && <div className={styles.quoteText}><RichText render={item.quote} /></div>}
+                  {/* {item.quote && <p className={styles.quoteText}>"{item.quote}"</p>} */}
+                </div>
+              </div>
+            </Container>
+          </div>
         </div>
-      </div>
-    ))}
+      ))}
     </>
   )
 }
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,16 +1,51 @@
+import { RichText } from "prismic-reactjs";
 import Layout from "../../components/Layout"
 import { Container } from "../../components/Grid"
 import Content from "./Content/Index"
 
 import styles from "../../styles/blog/blog-post.module.scss"
 
-import { getBlogData } from "../../lib/api"
+import { getBlogData, getBlogPostData, getMenuData } from "../../lib/api"
+
+const Post = ({
+  preview,
+  pageData,
+  mainMenuData,
+  footerMenuData,
+  tertiaryMenuData,
+}) => {
+  let date = pageData.date
+  let dateFormat = date.replace(/-/g, '.')
+
+  return (
+    <Layout
+      classNameVal={styles.blogPost}
+      title="Blog Post"
+      preview={preview}
+      mainMenuData={mainMenuData}
+      footerMenuData={footerMenuData}
+      tertiaryMenuData={tertiaryMenuData}
+    >
+      <div>
+        <Container>
+          <RichText render={pageData.title} />
+          <div className={styles.wrapper}>
+            <div className={styles.subtitle}>
+              <RichText render={pageData.subtitle} />
+            </div>
+            <p className={styles.date}>{dateFormat}</p>
+          </div>
+        </Container>
+        <Content props={pageData.content} />
+      </div>
+    </Layout>
+  );
+}
+
+export default Post;
 
 export const getStaticPaths = async (previewData) => {
   const pageData = await getBlogData(previewData);
-  // const res = await fetch('http://jsonplaceholder.typicode.com/posts');
-  // const data = await res.json();
-
 
   const paths = pageData.map(post => {
     return {
@@ -24,69 +59,20 @@ export const getStaticPaths = async (previewData) => {
   }
 }
 
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const res = await fetch('http://jsonplaceholder.typicode.com/posts/' + id);
-  const data = await res.json();
+export async function getStaticProps({ preview = false, previewData, params }) {
+  const pageData = await getBlogPostData(params.id, previewData);
+  const mainMenuData = await getMenuData("main-menu");
+  const footerMenuData = await getMenuData("footer-menu");
+  const tertiaryMenuData = await getMenuData("tertiary-menu");
 
   return {
-    props: { post: data }
+    props: {
+      preview,
+      pageData,
+      mainMenuData,
+      footerMenuData,
+      tertiaryMenuData,
+    },
+    revalidate: 1, // In seconds
   }
 }
-
-const content = [
-  {
-    image: '/images/blog/BlogPost/blog-post-image-1-min.jpg',
-    heading: 'Unlocking your potential',
-    p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    quote: 'The COR console provides you with previously unattainable data about your self health, on your terms, in your own home.'
-  },
-  {
-    image: '/images/blog/BlogPost/blog-post-image-2-min.jpg',
-    heading: 'Understanding your data',
-    p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    quote: 'Lorem ipsum dolor sit amet, conse ctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  },
-  {
-    heading: 'Adapting your lifestyle',
-    p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  }
-]
-
-
-const Post = ({ post, props }) => {
-  return (
-    <Layout
-      classNameVal={styles.blogPost}
-      title="Blog Post"
-    >
-      <div>
-        <Container>
-          <h1 className={styles.title}>Optimizing your health</h1>
-          <div className={styles.wrapper}>
-            <p className={styles.subtitle}>Daily Practices</p>
-            <p className={styles.date}>05.15.2021</p>
-          </div>
-        </Container>
-        <Content props={content} />
-      </div>
-    </Layout>
-  );
-}
-
-export default Post;
-
-// export async function getStaticProps({ preview = false, previewData, params = "optimizing-your-health" }) {
-//   const pageData = await getBlogData(params, previewData);
-//   const res = await fetch('http://jsonplaceholder.typicode.com/posts');
-//   const data = await res.json();
-
-//   return {
-//     props: {
-//       preview,
-//       pageData,
-//       posts: data.slice(0, 4)
-//     },
-//     revalidate: 1, // In seconds
-//   }
-// }
