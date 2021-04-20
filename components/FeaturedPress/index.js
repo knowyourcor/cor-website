22aa22dc82ab0be596b6064796c920a1d83baa64
maricon-upliftcx <maricon@upliftcx.com>
2021-04-20 17:57:13 +0800

    Featured Press

--- components/FeaturedPress/index.js
+++ components/FeaturedPress/index.js
@@ -1,8 +1,10 @@
+import { useState } from "react"
 import Link from "next/link"
 import { RichText } from "prismic-reactjs";
 import moment from "moment"
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
+import { motion } from "framer-motion";
 
 import Section from "../Section";
 import { Container, Row, Column } from "../Grid";
@@ -11,8 +13,8 @@ import styles from "./index.module.scss"
 
 
 const POSTS_QUERY = gql`
-query Categories($after: String) {
-  allBlog_posts(sortBy:date_ASC, first: 100, after: $after) {
+query FeaturedPress($after: String) {
+  allBlog_posts(first: 3, after: $after, where: { category_fulltext: "Featured Press" }) {
     pageInfo {
       hasPreviousPage
       hasNextPage
@@ -43,41 +45,80 @@ query Categories($after: String) {
 }
 `;
 
-export default function Index({ blogData }) {
+export default function Index() {
+  const [isHovered, setHovered] = useState(false)
+  const [activeItem, setActiveItem] = useState("item-0");
+
   const { data, loading, error, fetchMore } = useQuery(POSTS_QUERY, {
     variables: {
       after: null
     }
   });
 
-  let arrFilter = []
-
-  if (!loading) {
-    data.allBlog_posts.edges.forEach(element => {
-      if (element.node.category[0].text === 'Featured Press') {
-        arrFilter.push(element)
-      }
-    })
+  if (loading) {
+    return (
+      <Section>
+        <Container>
+          <h2 className="loading">Loading...</h2>
+        </Container>
+      </Section>
+    )
   }
-  // console.log(fetchMore)
-  // console.log(arrFilter)
-  // console.log(data)
-  // let arrFilter = []
 
-  // for (let blog = 0; blogData.length <= 2; blog++) {
-  //   // Runs 5 times, with values of step 0 through 4.
-  //   console.log('Walking east one step');
-  // }
+  if (error) {
+    console.error(error);
+    return null;
+  }
 
+  const handleShowMore = () => {
+    if (data.allBlog_posts.pageInfo.hasNextPage) {
+      const { endCursor } = data.allBlog_posts.pageInfo;
 
-  // console.log(arrFilter)
+      fetchMore({
+        variables: { after: endCursor, offset: 0, limit: 6 },
+
+        updateQuery: (prevResult, { fetchMoreResult }) => {
+          fetchMoreResult.allBlog_posts.edges = [
+            ...prevResult.allBlog_posts.edges,
+            ...fetchMoreResult.allBlog_posts.edges
+          ];
+          return fetchMoreResult;
+        }
+      });
+    }
+  };
+
+  const posts = data.allBlog_posts.edges;
+
+  const transition = {
+    opacity: {
+      duration: 0.45,
+      ease: [0.465, 0.183, 0.153, 0.946],
+    },
+    y: {
+      duration: 0.75,
+      ease: [0.465, 0.183, 0.153, 0.946],
+    },
+  };
+
+  const svgVariants = {
+    open: {
+      x: 50,
+      opacity: 1,
+      transition: transition,
+    },
+    closed: {
+      x: 0,
+      transition: transition,
+    },
+  }
 
   return (
     <Section className={styles.featuredWrap}>
       <Container>
         <h2>Featured press</h2>
         <div className={styles.featuredList}>
-          {arrFilter?.slice(0, 3).map((item, i) => {
+          {posts?.map((item, i) => {
             let pathNode = item.node
             let pathNodeContent = pathNode.content[0]
             let date = moment(item.node.date).format('LL')
@@ -93,18 +134,36 @@ export default function Index({ blogData }) {
                     <span>{date}</span>
                     <Link href={"/blog/" + pathNode._meta.uid}><a><RichText render={pathNode.title} /></a></Link>
                     {pathNodeContent?.link_name && (
-                      <Link href={"/blog/" + pathNode._meta.uid}>
-                        <a>
-                          <RichText render={pathNodeContent?.link_name} />
-                          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 512 512">
-                            <g>
+                      <motion.div
+                        className={styles.actionWrap}
+                        onMouseEnter={() => {
+                          setHovered(true)
+                          setActiveItem(`item-${i}`)
+                        }}
+                        onMouseLeave={() => {
+                          setHovered(false)
+                          setActiveItem(`item-${i}`)
+                        }}
+                      >
+                        <Link
+                          href={"/blog/" + pathNode._meta.uid}
+                        >
+                          <a>
+                            <RichText render={pathNodeContent?.link_name} />
+                            <motion.svg viewBox="0 0 512 512"
+                              initial={false}
+                              animate={isHovered && `item-${i}` === activeItem ? "open" : "closed"}
+                              variants={svgVariants}
+                            >
                               <g>
-                                <path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068 c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557 l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104 c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z" />
+                                <g>
+                                  <path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068 c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557 l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104 c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z" />
+                                </g>
                               </g>
-                            </g>
-                          </svg>
-                        </a>
-                      </Link>
+                            </motion.svg>
+                          </a>
+                        </Link>
+                      </motion.div>
                     )}
                   </Column>
                 </Row>
@@ -112,7 +171,17 @@ export default function Index({ blogData }) {
             )
           })}
         </div>
-        <Link href="/blog"><a className="btn btn--inverted">Show More</a></Link>
+        {data.allBlog_posts.pageInfo.hasNextPage ? (
+          <div className={styles.buttonHolder}>
+            <button className="btn btn--inverted" onClick={handleShowMore}>
+              Show More
+            </button>
+          </div>) : (
+          <Link href="/blog"><a className="btn btn--inverted">View More Blogs</a></Link>
+        )
+        }
+
+        {/* <Link href="/blog"><a className="btn btn--inverted">Show More</a></Link> */}
       </Container>
     </Section>
   )
