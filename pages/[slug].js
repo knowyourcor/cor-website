aeaa034286c1cde1ee08f076a2d8430239c4be2d
maricon-upliftcx <maricon@upliftcx.com>
2021-04-20 15:53:27 +0800

    FeaturedPress , QA Issues

--- pages/[slug].js
+++ pages/[slug].js
@@ -64,7 +64,7 @@ export default function Page({
           tertiaryMenuData={tertiaryMenuData}
         >
           <Modules pageData={pageData} />
-          {/* <FeaturedPress /> */}
+          <FeaturedPress />
         </Layout>
       )}
     </>
