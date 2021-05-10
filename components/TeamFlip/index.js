5109ae003409f77bb130f7c6bb9bcf9eae8676d9
kaye-upliftcx <kaye@upliftcx.com>
2021-05-10 14:47:36 +0800

    About: Our team, react modal on desktop to mobile

--- components/TeamFlip/index.js
+++ components/TeamFlip/index.js
@@ -1,34 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
-import ReactDOM from 'react-dom';
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { RichText } from "prismic-reactjs";
-import Modal from 'react-modal';
 import Picture from "../Picture";
 import { Column, Container, Row } from "../Grid";
+import TeamModal from "../TeamModal";
 
 import styles from "./index.module.scss";
 
-const CloseIcon = () => (
-  <svg
-    viewBox="0 0 365.696 365.696"
-    xmlns="http://www.w3.org/2000/svg"
-    className={styles.icon__close}
-  >
-    <path d="m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0" />
-  </svg>
-);
-
-const customModalInfo = {
-  overlay : {
-    zIndex: '1000',
-  },
-};
 
 const TeamList = ({ name, position, description, image }) => {
   const [activeFlip, setActiveFlip] = useState(false);
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const { ref, inView } = useInView({
     threshold: 0,
     triggerOnce: true,
@@ -52,18 +36,12 @@ const TeamList = ({ name, position, description, image }) => {
   };
 
   const openModal = () => {
-    setIsOpen(true);
+    setModalOpen(true);
   }
 
   const closeModal = () => {
-    setIsOpen(false);
+    setModalOpen(false);
   }
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      Modal.setAppElement(document.getElementById('#modalInfo'));
-    }
-  }, [])
 
   return (
     <Column columns={{ xs: 14, sm: 4 }}>
@@ -94,36 +72,14 @@ const TeamList = ({ name, position, description, image }) => {
             </div>
           </div>
         </div>
-        <Modal
-          id="#modalInfo"
-          className={styles.modalInfo}
-          style={customModalInfo}
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          contentLabel="Info Modal"
-          ariaHideApp={false}
-        >
-          <div className={styles.modalWrapper}>
-            <div className={styles.modalHeader}>
-              <div className={styles.avatar}>
-                <img src={image.url} />
-              </div>
-              <div className={styles.info}>
-                <RichText render={name} />
-                <RichText render={position} />
-              </div>
-              <span
-                className={styles.closeIcon}
-                onClick={closeModal}
-              >
-                <Image src="/icons/close.svg" height={25} width={25} />
-              </span>
-            </div>
-            <div className={styles.modalContent}>
-              <RichText render={description} />
-            </div>
-          </div>
-        </Modal>
+        <TeamModal
+          modalIsOpen={modalOpen}
+          close={closeModal}
+          image={image}
+          name={name}
+          position={position}
+          description={description}
+        />
       </motion.div>
     </Column>
   );
