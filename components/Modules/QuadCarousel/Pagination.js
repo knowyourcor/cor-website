7ca79fc5bcc09a3940dfafb8856b88763dc896e9
Nathan Searles <nsearles@gmail.com>
2021-05-10 15:54:48 -0700

    Resolved key and className warnings

--- components/Modules/QuadCarousel/Pagination.js
+++ components/Modules/QuadCarousel/Pagination.js
@@ -1,13 +1,13 @@
 import styles from "./quadCarousel.module.scss";
 
 export default function Pagination({ items, active, setActive }) {
-  console.log(items);
   return (
     <ul className={styles.pagination}>
       {items.map((item, index) => (
         <li
-          className={active === index + 1 && styles.active}
+          className={active === index + 1 ? styles.active : ""}
           onClick={() => setActive(index + 1)}
+          key={`item_${item.id}`}
         >
           <span></span>
         </li>
