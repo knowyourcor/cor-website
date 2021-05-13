bab19bd0bf89c5a5b7410cb21ade44ff751d5109
Nathan Searles <nsearles@gmail.com>
2021-05-12 17:11:21 -0700

    Added support for a score to the roundel

--- components/Roundel/index.js
+++ components/Roundel/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import { RichText } from "prismic-reactjs";
 import { motion, useAnimation } from "framer-motion";
+import styles from "./roundel.module.scss";
 
-export default function Roundel({ index = 0 }) {
+export default function Roundel({ index = 0, score }) {
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -10,6 +12,7 @@ export default function Roundel({ index = 0 }) {
 
   const transition = { duration: 4, ease: "anticipate" };
 
+  const scaleUp = useAnimation();
   const red = useAnimation();
   const green = useAnimation();
   const blue = useAnimation();
@@ -17,6 +20,11 @@ export default function Roundel({ index = 0 }) {
 
   // Set positions on intital load
   useEffect(() => {
+    scaleUp.set({
+      scale: 0.35,
+      opacity: 0,
+    });
+
     red.set({
       rotate: getRandomInt(0, 360),
       strokeDashoffset: getRandomInt(220, 300),
@@ -40,6 +48,12 @@ export default function Roundel({ index = 0 }) {
 
   // Update position on data change
   useEffect(() => {
+    scaleUp.start({
+      scale: 1,
+      opacity: 1,
+      transition: { delay: 2, duration: 0.5, ease: "easeOut" },
+    });
+
     red.start({
       rotate: getRandomInt(0, 360),
       strokeDashoffset: getRandomInt(220, 300),
@@ -65,63 +79,70 @@ export default function Roundel({ index = 0 }) {
     });
   }, [index]);
   return (
-    <svg viewBox="0 0 148 148" xmlns="http://www.w3.org/2000/svg">
-      <g transform="translate(24 24)" fill="none" fillRule="evenodd">
-        {/* Gray */}
-        <circle stroke="#E5E0DF" strokeWidth="2" cx="50" cy="50" r="41.75" />
+    <>
+      <svg viewBox="0 0 148 148" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(24 24)" fill="none" fillRule="evenodd">
+          {/* Gray */}
+          <circle stroke="#E5E0DF" strokeWidth="2" cx="50" cy="50" r="41.75" />
 
-        {/* Blue */}
-        <motion.circle
-          stroke="#034069"
-          strokeWidth="29"
-          cx="50"
-          cy="50"
-          r="55.242"
-          animate={blue}
-          style={{
-            strokeDasharray: 360,
-          }}
-        />
+          {/* Blue */}
+          <motion.circle
+            stroke="#034069"
+            strokeWidth="29"
+            cx="50"
+            cy="50"
+            r="55.242"
+            animate={blue}
+            style={{
+              strokeDasharray: 360,
+            }}
+          />
 
-        {/* Teal */}
-        <motion.circle
-          stroke="#80DEEB"
-          strokeWidth="29"
-          cx="50"
-          cy="50"
-          r="55.242"
-          animate={teal}
-          style={{
-            strokeDasharray: 360,
-          }}
-        />
+          {/* Teal */}
+          <motion.circle
+            stroke="#80DEEB"
+            strokeWidth="29"
+            cx="50"
+            cy="50"
+            r="55.242"
+            animate={teal}
+            style={{
+              strokeDasharray: 360,
+            }}
+          />
 
-        {/* Green */}
-        <motion.circle
-          stroke="#70DB8C"
-          strokeWidth="19"
-          cx="50"
-          cy="50"
-          r="50.242"
-          animate={green}
-          style={{
-            strokeDasharray: 360,
-          }}
-        />
+          {/* Green */}
+          <motion.circle
+            stroke="#70DB8C"
+            strokeWidth="19"
+            cx="50"
+            cy="50"
+            r="50.242"
+            animate={green}
+            style={{
+              strokeDasharray: 360,
+            }}
+          />
 
-        {/* Red */}
-        <motion.circle
-          stroke="#FF7050"
-          strokeWidth="19"
-          cx="50"
-          cy="50"
-          r="50.242"
-          animate={red}
-          style={{
-            strokeDasharray: 360,
-          }}
-        />
-      </g>
-    </svg>
+          {/* Red */}
+          <motion.circle
+            stroke="#FF7050"
+            strokeWidth="19"
+            cx="50"
+            cy="50"
+            r="50.242"
+            animate={red}
+            style={{
+              strokeDasharray: 360,
+            }}
+          />
+        </g>
+      </svg>
+      {score && (
+        <motion.div animate={scaleUp} className={styles.score}>
+          <RichText render={score} />
+        </motion.div>
+      )}
+    </>
   );
 }
