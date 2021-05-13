3431a6899135e5dcf2d23550c6483eb99de50263
Nathan Searles <nsearles@gmail.com>
2021-05-13 13:39:25 -0700

    Shop > Product: clean up

--- components/Modules/Product/index.js
+++ components/Modules/Product/index.js
@@ -1,40 +1,12 @@
 import { RichText } from "prismic-reactjs";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
-
 import Section from "../../Section";
 import { Column, Container, Row } from "../../Grid";
 import Picture from "../../Picture";
+import Button from "../../Button";
 
 import styles from "./index.module.scss";
-
-export const linkResolver = (doc) => {
-  // URL for a category type
-  if (doc.type === "category") {
-    return `/category/${doc.uid}`;
-  }
-
-  // URL for a product type
-  if (doc.type === "product") {
-    return `/product/${doc.uid}`;
-  }
-
-  // URL for a page type
-  if (doc.type === "page") {
-    return `/${doc.uid}`;
-  }
-
-  if (doc.type === "shop") {
-    return `/${doc.uid}`;
-  }
-
-  if (doc.link_type === "Document") {
-    return `/${doc.uid}`;
-  }
-
-  // Backup for all other types
-  return "/";
-};
 
 export default function Index({ primary }) {
   const { ref, inView } = useInView({
@@ -59,8 +31,6 @@ export default function Index({ primary }) {
     },
   };
 
-  console.log(primary);
-
   return (
     <Section className={styles.productSection}>
       <Container>
@@ -71,13 +41,8 @@ export default function Index({ primary }) {
           exit="hidden"
           variants={variants}
         >
-          <Row align="center">
-            <Column
-              columns={{ xs: 14, md: 7 }}
-              offsets={{ md: 1 }}
-              justify="center"
-              className="custom__column"
-            >
+          <Row alig={{ xs: "center" }}>
+            <Column columns={{ xs: 14, md: 6 }} offsets={{ md: 1 }}>
               <div className={styles.productImage}>
                 <Picture image={primary.image} classes={styles.image} />
                 <Picture
@@ -86,11 +51,7 @@ export default function Index({ primary }) {
                 />
               </div>
             </Column>
-            <Column
-              columns={{ xs: 14, md: 5 }}
-              offsets={{ md: 1 }}
-              justify="center"
-            >
+            <Column columns={{ xs: 14, md: 5 }} offsets={{ md: 1 }}>
               <RichText render={primary.product_details} />
               <div className={styles.productPurchase}>
                 <div className={styles.productPriceLabel}>
@@ -104,7 +65,10 @@ export default function Index({ primary }) {
                 </div>
               </div>
               <div className={styles.button}>
-                <RichText render={primary.link} linkResolver={linkResolver} />
+                <Button
+                  linkData={primary.link}
+                  labelData={primary.link_label}
+                />
               </div>
               <div className={styles.secondaryDescription}>
                 <RichText render={primary.secondary_description} />
