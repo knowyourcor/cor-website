b5d4153d79aca00a3fec1f61ec37bfad72ed5769
Nathan Searles <nsearles@gmail.com>
2021-05-13 17:12:08 -0700

    Shop text + background refinement

--- components/Modules/ShopTextBackgroundImage/index.js
+++ components/Modules/ShopTextBackgroundImage/index.js
@@ -1,13 +1,13 @@
 import { RichText } from "prismic-reactjs";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
-
+import Picture from "../../Picture";
 import Section from "../../Section";
 import { Container, Row, Column } from "../../Grid";
 
-import styles from "./index.module.scss";
+import styles from "./shopTextBackgroundImage.module.scss";
 
-export default function Index({ primary }) {
+export default function ShopTextBackgroundImage({ primary }) {
   const { ref, inView } = useInView({
     threshold: 0,
     triggerOnce: true,
@@ -35,30 +35,33 @@ export default function Index({ primary }) {
       className={styles.textBackgroundImage}
       style={{ backgroundColor: primary.background_color }}
     >
-      <Container>
-        <Row>
-          <Column
-            columns={{ xs: 14, md: 6 }}
-            offsets={{ md: 1, lg: 1 }}
-            className="custom__column"
-          >
+      <Container ref={ref}>
+        <Row align={{ xs: "center" }}>
+          <Column columns={{ xs: 14, md: 4 }} offsets={{ md: 1 }}>
             <motion.div
-              ref={ref}
               initial="hidden"
               animate={inView ? "show" : "hidden"}
               exit="hidden"
               variants={variants}
+              className={styles.content}
             >
-              <RichText render={primary.heading} />
+              <RichText render={primary.headline} />
               <RichText render={primary.text} />
+            </motion.div>
+          </Column>
+          <Column columns={{ xs: 14, md: 9 }}>
+            <motion.div
+              initial="hidden"
+              animate={inView ? "show" : "hidden"}
+              exit="hidden"
+              variants={variants}
+              className={styles.image}
+            >
+              <Picture image={primary.image} />
             </motion.div>
           </Column>
         </Row>
       </Container>
-      <div
-        className={styles.imageWrap}
-        style={{ backgroundImage: `url(${primary.background_image.url})` }}
-      ></div>
     </Section>
   );
 }
