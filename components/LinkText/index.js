219299440389cbfdeb0f0daa784dadc46628c5cc
Nathan Searles <nsearles@gmail.com>
2021-05-14 20:44:36 -0700

    TextDownloader refinement

---
+++ components/LinkText/index.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+const LinkText = ({ linkData, labelData }) => {
+  const WebLink = (link, label) => {
+    return (
+      <a
+        href={link?.url}
+        title={label[0].text}
+        target={link?.target}
+        rel="noopener noreferrer"
+      >
+        {label[0].text}
+      </a>
+    );
+  };
+
+  const PageLink = (link, label) => {
+    return (
+      <Link href="/[slug]" as={`/${link?._meta?.uid}`}>
+        <a>{label[0]?.text}</a>
+      </Link>
+    );
+  };
+
+  const LinkByType = ({ link, label }) => {
+    if (link?._linkType === "Link.web") {
+      return WebLink(link, label);
+    } else if (link?._linkType === "Link.document") {
+      return PageLink(link, label);
+    } else {
+      return null;
+    }
+  };
+
+  return <LinkByType link={linkData} label={labelData} />;
+};
+
+export default LinkText;
