075f9a35968b4752b847ced86d04697d94eee17a
Nathan Searles <nsearles@gmail.com>
2021-05-25 10:53:52 -0700

    Fixes SMCR-7, SMCR-8, SMCR-35

--- components/Modules/BodyText/index.js
+++ components/Modules/BodyText/index.js
@@ -1,29 +1,15 @@
 import { RichText } from "prismic-reactjs";
-import { useInView } from "react-intersection-observer";
-import { motion } from "framer-motion";
 import { Container, Row, Column } from "../../Grid";
-import { fadeIn } from "../../../lib/variants";
 import styles from "./text.module.scss";
 
 const BodyText = ({ primary }) => {
-  const { ref, inView } = useInView({
-    threshold: 0.2,
-    triggerOnce: true,
-  });
-
   return (
-    <Container ref={ref}>
+    <Container>
       <Row>
         <Column columns={{ xs: 14, sm: 12, md: 8 }} offsets={{ sm: 1, md: 3 }}>
-          <motion.div
-            ref={ref}
-            initial="hidden"
-            animate={inView ? "show" : "hidden"}
-            variants={fadeIn}
-            className={styles.text}
-          >
+          <div className={styles.text}>
             <RichText render={primary.text} />
-          </motion.div>
+          </div>
         </Column>
       </Row>
     </Container>
