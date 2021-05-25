6ca854019224a105049f796cbdf99959ce6a1f46
Nathan Searles <nsearles@gmail.com>
2021-05-25 16:02:54 -0700

    fixes SMCR-28

--- components/Modules/QuadCarousel/Pagination.js
+++ components/Modules/QuadCarousel/Pagination.js
@@ -4,12 +4,12 @@ export default function Pagination({ items, active, setActive }) {
   return (
     <ul className={styles.pagination}>
       {items.map((item, index) => (
-        <li
-          className={active === index + 1 ? styles.active : ""}
-          onClick={() => setActive(index + 1)}
-          key={`item_${item.id}`}
-        >
-          <span></span>
+        <li key={`item_${item.id}`}>
+          <button
+            className={active === index + 1 ? styles.active : ""}
+            onClick={() => setActive(index + 1)}
+            aria-label={`Select item number ${index + 1}`}
+          ></button>
         </li>
       ))}
     </ul>
