48af183d8be23b4f713c407059c81c9ac70785ca
Nathan Searles <nsearles@gmail.com>
2021-05-25 17:01:53 -0700

    Fixes SMCR-39

--- components/EmailSignup/index.js
+++ components/EmailSignup/index.js
@@ -8,6 +8,7 @@ const url =
 const CustomForm = ({ status, message, onValidated, onSuccess, theme }) => {
   const emailRef = useRef();
   const [isValid, setIsValid] = useState(true);
+  const [inputValue, setInputValue] = useState("");
 
   const handleSubmit = () => {
     const email = emailRef.current;
@@ -38,6 +39,7 @@ const CustomForm = ({ status, message, onValidated, onSuccess, theme }) => {
 
   useEffect(() => {
     return () => {
+      setInputValue("");
       window.removeEventListener("keypress", handleInputFocus);
     };
   }, []);
@@ -80,6 +82,8 @@ const CustomForm = ({ status, message, onValidated, onSuccess, theme }) => {
             onBlur={handleInputBlur}
             onFocus={handleInputFocus}
             aria-label="Enter email address"
+            onChange={(e) => setInputValue(e.currentTarget.value)}
+            value={inputValue}
           />
           <button
             onClick={handleSubmit}
