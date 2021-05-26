36d78a9fcc87523fe77656aacf2b1974067d351c
Nathan Searles <nsearles@gmail.com>
2021-05-26 15:12:44 -0700

    Updated to Prismic preview feature

--- components/Alert/index.js
+++ components/Alert/index.js
@@ -1,16 +1,19 @@
+import { useRouter } from "next/router";
 import styles from "./alert.module.scss";
 
-export default function Alert({ preview }) {
+export default function Alert({ children }) {
+  const { isPreview } = useRouter();
   return (
-    <>
-      {preview && (
-        <div className={styles.alert}>
-          <p>
-            This is page is a preview.{" "}
-            <a href="/api/exit-preview">Click here</a> to exit preview mode.
-          </p>
-        </div>
-      )}
-    </>
+    <div className={styles.alert}>
+      {children}
+      {isPreview ? (
+        <>
+          <p>You're viewing a preview of this page</p>
+          <a href="/api/exit-preview" className={styles.button}>
+            Exit Preview
+          </a>
+        </>
+      ) : null}
+    </div>
   );
 }
