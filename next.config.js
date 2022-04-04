ee60294aeeafbbcebfe48b0e8e10522547752460
Jonah Madeya <jonah@thecoolagency.com>
2022-04-04 15:05:36 -0700

    added redirect for flexibility with nav links

--- next.config.js
+++ next.config.js
@@ -27,6 +27,16 @@ module.exports = {
         destination: "/",
         permanent: false,
       },
+      {
+        source: "/shop",
+        destination: "https://shop.thecor.com/products/cor-pioneer?_ga=2.118826441.1364353575.1649026903-1546752094.1648774141",
+        permanent: false,
+      },
+      {
+        source: "/invest",
+        destination: "https://wefunder.com/cor",
+        permanent: true,
+      },
     ];
   },
 };
